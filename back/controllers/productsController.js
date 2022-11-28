1b98d2ee190c4387dfdedf1201e8406928266ba0
ArmandoColMen <87383495+ArmandoColMen@users.noreply.github.com>
2022-11-28 14:31:08 -0500

    Semana1

---
+++ back/controllers/productsController.js
@@ -0,0 +1,8 @@
+/* Ver la lista de productos */
+exports.getProducts = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "En esta ruta ud va a poder ver todos los productos"
+  })
+}
+/* Ver la lista de productos */
